7c0634473ada641d7c3f92def3364db804abf0bb
alanteenageorge3899-ui <alanteenageorge3899@gmail.com>
2026-07-16 05:50:25 +0530

    Exercise 3 - Assertions in JUnit

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,18 +2,45 @@ package com.cognizant;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
-    void testAddition() {
+    void testAssertEquals() {
+        assertEquals(15, calculator.add(10, 5));
+    }
 
-        Calculator calculator = new Calculator();
+    @Test
+    void testAssertNotEquals() {
+        assertNotEquals(20, calculator.subtract(10, 5));
+    }
 
-        int result = calculator.add(10, 20);
+    @Test
+    void testAssertTrue() {
+        assertTrue(calculator.multiply(4, 5) == 20);
+    }
 
-        assertEquals(30, result);
+    @Test
+    void testAssertFalse() {
+        assertFalse(calculator.divide(20, 5) == 3);
+    }
 
+    @Test
+    void testAssertNull() {
+        assertNull(calculator.getNullObject());
+    }
+
+    @Test
+    void testAssertNotNull() {
+        assertNotNull(calculator.getObject());
+    }
+
+    @Test
+    void testAssertSame() {
+        String text = calculator.getMessage();
+        assertSame(text, text);
     }
 }
